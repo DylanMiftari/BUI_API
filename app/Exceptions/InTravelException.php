2ec98c620387f85c3e70820b34b804ccde01099c
DylanMiftari <mifdylan511@gmail.com>
2025-12-24 16:15:18 +0100

    fix: Edit in travel middleware

--- app/Exceptions/InTravelException.php
+++ app/Exceptions/InTravelException.php
@@ -4,10 +4,19 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Request;
 
 class InTravelException extends AuthorizationException
 {
     public function __construct() {
         parent::__construct("You can't do this action during a travel");
+    }
+
+    public function render(Request $request)
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+            'error'   => 'InTravelError'
+        ], 409);
     }
 }
--- app/Http/Middleware/InTravelMiddleware.php
+++ app/Http/Middleware/InTravelMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Exceptions\InTravelException;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,7 +18,14 @@ class InTravelMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth::user()->inTravel) {
+        $user = Auth::user();
+        if($user->inTravel) {
+            $endTravel = Carbon::createFromFormat("Y-m-d H:i:s", $user->endTravel);
+            if($endTravel->isBefore(Carbon::now())) {
+                $user->inTravel = false;
+                $user->save();
+                return $next($request);
+            }
             throw new InTravelException();
         }
         return $next($request);
